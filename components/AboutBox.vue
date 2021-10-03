bdb20fa011e0530e1459a0fced463b9aa03b6b0e
Linus Kinzel <linus@dahmakan.com>
2021-10-03 14:32:05 +0800

    layout

--- components/AboutBox.vue
+++ components/AboutBox.vue
@@ -5,7 +5,8 @@
 		</div>
 		<p>
 			<br>
-			Hey. I'm Head of Product at <a href="https://popmeals.com.my" target="_blank">Pop Meals (formerly dahmakan)</a>, where we're building Southeast Asia's leading Food Tech company. I also do web3 and Solidity projects on the side, for example <a href="http://smartpools.netlify.app" target="_blank">a decentralized, trustless investment pooling platform.</a>
+			Hey. I'm Head of Product at <a href="https://popmeals.com.my" target="_blank">Pop Meals (formerly dahmakan)</a>, where we're building Southeast Asia's leading Food Tech company. 
+			<br /><br />I also do web3 and Solidity projects on the side, for example <a href="http://smartpools.netlify.app" target="_blank">a decentralized, trustless investment pooling platform.</a>
 			<br><br>
 			Talk to me
 			<div class="buttons">
